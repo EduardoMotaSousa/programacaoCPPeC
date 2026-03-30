bfc88a56e9e01e86d3b8bcfa0ed1df1a1e3e5c8e
Eduardo Mota <134015632+EduardoMotaSousa@users.noreply.github.com>
2026-03-30 22:23:00 +0000

    Ex 1 ao 9 em C++ Resposta dos exercicios do professor Douglas do 1 ao 9 em C++

--- Modulo06/exercicios/output/alomundo.c
+++ Modulo06/exercicios/output/alomundo.c
@@ -4,8 +4,8 @@ Faça um Programa que mostre a mensagem "Alo mundo" na tela.
 #include <stdio.h>
 #include <ctype.h>
 int main(){
-    printf("\nAlo mundo\n");
-    printf("\nAlo mundo\n");
-    printf("\nAlo mundo\n");
+    printf("\nAlo Mundo\n");
+    printf("\nAlo Mundo\n");
+    printf("\nAlo Mundo\n");
     return 0;
 }
